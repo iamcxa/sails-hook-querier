a96fa7b6c43b24bc2e53e0a4a7fb6af3b4f81b07
rsghost <im@rsghost.org>
2021-05-18 12:18:47 +0000

    test: 💍 QueryHelper create

--- test/unit/services/QueryHelper.spec.js
+++ test/unit/services/QueryHelper.spec.js
@@ -1,3 +1,27 @@
+const fixtures = require('./../../fixtures/instances.json');
+
 describe('about NotifierHelper operation.', () => {
-  let user;
+  it('create should be success', async () => {
+    const result = await QueryHelper.default.create({
+      modelName: 'User',
+      include: [],
+      input: fixtures.user,
+    }, {
+      formatCb: e => e,
+    });
+
+    const target = {
+      ...fixtures.user,
+      id: 0,
+      updatedAt: new Date(),
+      createdAt: new Date(),
+    };
+
+    SpecHelper.validateEach({
+      source: result.dataValues,
+      target,
+    }, {
+      strictMode: false,
+    });
+  });
 });
