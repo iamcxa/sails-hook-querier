3cd1ef03605c43f7377ba2102e9415985a9c1a45
Kent <iamcxa@gmail.com>
2021-05-19 10:49:19 +0800

    refactor: 💡 clean db schema before any test cases

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -1,24 +1,37 @@
 const chai = require('chai');
 chai.use(require('chai-datetime'));
+
 global.should = chai.should();
 global.sinon = require('sinon');
 global.SpecHelper = require('../node_modules/sails-hook-blocks/api/services/helpers/SpecHelper');
-before(function (done) {
-  let rc;
-  const sails = require('./fixtures/app').sails;
 
+before(function (done) {
   // Hook will timeout in 10 seconds
   this.timeout(11000);
 
-  rc = require('rc');
+  const Sequelize = require('sequelize');
+  const connInfo = require('./fixtures/config/datastores').datastores.default;
+  const connection = new Sequelize(connInfo.url, connInfo.options);
 
-  const config = rc('sails');
-  config.hooks.sequelize = require('../node_modules/sails-hook-sequelize/index');
-  config.hooks.querier = require('../index');
+  // Drop schemas if exists
+  connection.query('SET FOREIGN_KEY_CHECKS=0;').then(() =>
+    connection.query('DROP SCHEMA IF EXISTS sails;').then(() =>
+      connection.query('SET FOREIGN_KEY_CHECKS=1;').then(() => {
+        const Sails = require('./fixtures/app').sails;
+        const rc = require('rc');
+        const config = rc('sails');
+        config.hooks.sequelize = require('../node_modules/sails-hook-sequelize/index');
+        config.hooks.querier = require('../index');
 
-  // Attempt to lift sails
-  sails().lift(config, (err, Sails) => {
-    if (err) { return done(err); }
-    return done(err, Sails);
-  });
+        // Attempt to lift sails
+        Sails().lift(config, (err, _sails) => {
+          if (err) {
+            return done(err);
+          }
+          sails = _sails;
+          return done(err, sails);
+        });
+      }),
+    ),
+  );
 });
