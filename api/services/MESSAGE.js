5db669a351af34899dea2cbe878278a51c8debff
rsghost <im@rsghost.org>
2021-05-26 12:18:33 +0000

    style: 💄 remove useless comments

--- api/services/MESSAGE.js
+++ api/services/MESSAGE.js
@@ -1,10 +1,3 @@
-/****************************************************************************
-*                                                                           *
-* Export custom response messages to global accessible, so you can use them *
-* like this: MESSAGE.BAD_REQUEST.NO_TARGET_FOUNDED('something not found').  *
-*                                                                           *
-/****************************************************************************/
-
 module.exports = {
   ...sails.config.messages,
 };
