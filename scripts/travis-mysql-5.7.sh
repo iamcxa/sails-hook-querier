4a83eaec24aa72e21de4524189e5806081a8da26
Kent <iamcxa@gmail.com>
2021-05-31 01:31:21 +0800

    ci: 🎡 add scripts

---
+++ scripts/travis-mysql-5.7.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+# MySQL config
+mysql -uroot -e "
+set character_set_client      = utf8mb4;                       
+set character_set_connection  = utf8mb4;                       
+set character_set_database    = utf8mb4;                       
+set character_set_results     = utf8mb4;                       
+set character_set_server      = utf8mb4;
+SHOW VARIABLES WHERE Variable_name LIKE 'character%' OR Variable_name LIKE 'collation%';
+CREATE DATABASE \`sails-test\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+SHOW DATABASES;
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+"
+exit
